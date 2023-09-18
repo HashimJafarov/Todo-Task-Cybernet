354c1626f96eb0c38a2c5b44f36a74b23b352777
Your NameHashimJafarov <hashim.cfr@gmail.com>
2023-09-19 00:07:12 +0400

    css and js almost done

--- app.js
+++ app.js
@@ -1,9 +1,130 @@
 const input = document.getElementById("todo_input");
 const btn = document.getElementById("btn");
-let todos = [];
+const result = document.getElementById("todo_result");
+const error = document.getElementById("error");
+let allTodos = [];
+let newTodo = {
+  id: 0,
+  todo: "",
+  completed: false,
+};
+const makeLoading = () => {
+  result.textContent = "";
+  const loading = document.createElement("div");
+  loading.classList.add("loading");
+  result.style.display = "flex";
+  result.style.justifyContent = "center";
+  result.style.alignItems = "center";
+  result.append(loading);
+};
 
-btn.addEventListener("click", (e) => {
-  let inputValue = input.value;
-  todos.push(inputValue);
-  console.log(todos);
+const removeLoading = () => {
+  const loading = document.querySelector("loading");
+  result.style.display = "block";
+  result.style.justifyContent = "";
+  result.style.alignItems = "";
+  if (loading) {
+    loading.style.display = "none";
+  }
+};
+const getTodos = () => {
+  makeLoading();
+  fetch("https://dummyjson.com/todos?limit=3")
+    .then((a) => a.json())
+    .then((a) => {
+      allTodos.push(a.todos);
+      console.log(a);
+      showTodo(allTodos);
+      removeLoading();
+    });
+};
+getTodos();
+
+const showTodo = (allTodos) => {
+  if (!allTodos.length) {
+    result.textContent = "";
+    let noResult = document.createElement("p");
+    noResult.textContent = "The ToDo List is Empty. Add some ToDo!";
+    result.append(noResult);
+    return;
+  }
+  result.textContent = "";
+  allTodos.map((todo) => {
+    let todoDiv = document.createElement("div");
+    let todoLabel = document.createElement("label");
+    todoLabel.setAttribute("for", `${todo.id}`);
+    let todoCheckbox = document.createElement("input");
+    todoCheckbox.setAttribute("type", "checkbox");
+    todoCheckbox.setAttribute("id", `${todo.id}`);
+    todoCheckbox.classList.add("hidden");
+    todoDiv.classList.add("todo_list");
+    let todoList = document.createElement("p");
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    todoList.textContent = todo.todo;
+    todoLabel.append(todoList);
+    todoDiv.append(todoCheckbox, todoLabel, deleteButton);
+    result.append(todoDiv);
+    todoLabel.addEventListener("click", () => {
+      todoChecked(todo, todoList);
+    });
+    deleteButton.addEventListener("click", (e) => {
+      deleteTodo(todo, allTodos);
+    });
+  });
+};
+const todoChecked = (todo, todoList) => {
+  todo.completed = true;
+  if (todo.completed) {
+    todoList.style.textDecoration = "line-through";
+  }
+};
+btn.addEventListener("click", () => {
+  if (input.value === "") {
+    error.style.display = "block";
+    error.style.color = "red";
+    error.textContent = "Error! Add some ToDo!";
+    input.style.borderColor = "red";
+    return;
+  }
+  input.style.borderColor = "#000";
+
+  let newTodo = {
+    id: allTodos.length + 1,
+    todo: input.value,
+    completed: false,
+    userId: 5,
+  };
+  addTodo(newTodo);
+  input.value = "";
 });
+
+const deleteTodo = (todo, allTodos) => {
+  makeLoading();
+  fetch(`https://dummyjson.com/todos/${todo.id}`, {
+    method: "DELETE",
+  })
+    .then((a) => a.json())
+    .then((a) => {
+      if (a.isDeleted) {
+        allTodos = allTodos.filter((a) => a.id !== todo.id);
+        showTodo(allTodos);
+        removeLoading();
+      }
+    });
+};
+
+const addTodo = (newTodo) => {
+  makeLoading();
+  fetch("https://dummyjson.com/todos/add", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newTodo),
+  })
+    .then((a) => a.json())
+    .then(() => {
+      allTodos.push(newTodo);
+      showTodo(allTodos);
+      removeLoading();
+    });
+};
