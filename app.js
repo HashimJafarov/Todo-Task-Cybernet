db730b1fc79a4c73278fe2b1b100ca5cbaf637dd
Your NameHashimJafarov <hashim.cfr@gmail.com>
2023-09-18 11:06:04 +0400

    project skeleton

---
+++ app.js
@@ -0,0 +1,8 @@
+const input = document.getElementById("todo_input");
+const btn = document.getElementById("btn");
+let todos = [];
+
+btn.addEventListener("click", (e) => {
+  let inputValue = input.value;
+  
+});
